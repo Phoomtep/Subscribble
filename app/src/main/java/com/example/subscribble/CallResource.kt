4dc7cde9ebaff8f56d01863242e360855d6277bd
Phoom <phoombnw@gmail.com>
2023-10-16 19:57:18 +0700

    Add CallResource - callsify - color

--- app/src/main/java/com/example/subscribble/CallResource.kt
+++ app/src/main/java/com/example/subscribble/CallResource.kt
@@ -1,5 +1,7 @@
 package com.example.subscribble
 
+import androidx.compose.ui.graphics.Color
+
 fun getDrawableResource(inputString: String): Int {
     return when (inputString) {
         "netflix" -> R.drawable.netflix
@@ -11,5 +13,34 @@ fun getDrawableResource(inputString: String): Int {
         "spotify" -> R.drawable.spotify
         "youtubemusic" -> R.drawable.youtubemusic
         else -> R.drawable.something_went_wrong
+    }
+}
+
+fun classify(inputString: String): String {
+    return when (inputString) {
+        "netflix" -> "video"
+        "appletv" -> "video"
+        "youtube" -> "video"
+        "disneyplus" -> "video"
+
+        "applemusic" -> "music"
+        "spotify" -> "music"
+        "youtubemusic" -> "music"
+        else -> ""
+    }
+}
+
+fun getApplicationColor(inputString: String): Color {
+    return when (inputString){
+        "netflix" -> Color(0xFFAB060F)
+        "appletv" -> Color(0xFF2D2D2D)
+        "youtube" -> Color(0xFFF70000)
+        "disneyplus" -> Color(0xFF0060DE)
+
+        "applemusic" -> Color(0xFFF74F6B)
+        "spotify" -> Color(0xFF1BC357)
+        "youtubemusic" -> Color(0xFFF70000)
+
+        else -> Color.White
     }
 }
